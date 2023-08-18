04e542b70993a030aa40fc6e2c1ef7a9e7acae6a
romisuman6201 <romisuman141@gmail.com>
2023-08-18 15:23:46 +0530

    ok

---
+++ COM.QSPIDERJAVA/src/COM/STRING/Git.java
@@ -0,0 +1,14 @@
+package COM.STRING;
+
+public class Git {
+	public void add() {
+		System.out.println("Add two Numbers");
+	}
+
+	public static void main(String[] args) {
+		Git g=new Git();
+		g.add();
+		
+	}
+
+}
---
+++ JenkinsD/src/test/java/DemoTest.java
@@ -0,0 +1,13 @@
+
+public class DemoTest {
+	public void test() {
+		System.out.println("test=======");
+	}
+
+	public static void main(String[] args) {
+		DemoTest d=new DemoTest();
+		d.test();
+
+	}
+
+}
